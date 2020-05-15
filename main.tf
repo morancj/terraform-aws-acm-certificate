0f43a3b7c9a80fa0f8ef88b670216504233d974a
Ciaran Moran <25228551+morancj@users.noreply.github.com>
2020-05-15 14:43:52 +0100

    Cert validation fix

--- main.tf
+++ main.tf
@@ -25,11 +25,11 @@ resource "aws_route53_record" "validation" {
   provider = aws.route53_account
   count    = length(var.subject_alternative_names) + 1
 
-  name    = lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_name")
-  type    = lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_type")
+  name    = aws_acm_certificate.default.domain_validation_options[count.index]["resource_record_name"]
+  type    = aws_acm_certificate.default.domain_validation_options[count.index]["resource_record_type"]
   zone_id = var.hosted_zone_id
   records = [
-    lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_value")
+    aws_acm_certificate.default.domain_validation_options[count.index]["resource_record_value"]
   ]
   ttl     = var.validation_record_ttl
 }
